c7e78386e0d8eb84279e865622c69e5fca6398e9
Serhii Muslanov <dikhims@gmail.com>
2019-04-10 00:21:53 +0300

    Some test methods

--- src/main/java/org/dikhim/spring/rest/persistence/UserRepository.java
+++ src/main/java/org/dikhim/spring/rest/persistence/UserRepository.java
@@ -1,7 +1,6 @@
 package org.dikhim.spring.rest.persistence;
 
 import org.dikhim.spring.rest.model.User;
-import org.graalvm.compiler.lir.LIRInstruction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/test/java/org/dikhim/spring/rest/controllers/TestUserController.java
@@ -0,0 +1,100 @@
+package org.dikhim.spring.rest.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.dikhim.spring.rest.model.User;
+import org.dikhim.spring.rest.service.UserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import javax.validation.constraints.Null;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class TestUserController {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+
+    private User user;
+    private List<User> users;
+
+    @Before
+    public void setUp() {
+        this.users = new ArrayList<>();
+        User user = new User();
+        user.setId(1);
+        user.setFirstName("Serhii");
+        user.setLastName("Muslanov");
+        this.user = user;
+        users.add(user);
+
+        user = new User();
+        user.setId(2);
+        user.setFirstName("Vladimir");
+        user.setLastName("Ivanov");
+        users.add(user);
+    }
+
+    @Test
+    public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
+        given(userService.findAll()).willReturn(users);
+
+        mvc.perform(get("/users")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].firstName").value(user.getFirstName()))
+                .andExpect(jsonPath("$[0].lastName").value(user.getLastName()));
+    }
+
+    @Test
+    public void whenPostUser_thenOk() throws Exception {
+        String jsonString = mapper.writeValueAsString(user);
+        mvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status()
+                        .isAccepted());
+    }
+
+    @Test
+    public void getUserById() {
+    }
+
+    @Test
+    public void put() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
